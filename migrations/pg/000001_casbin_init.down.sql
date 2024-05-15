019d7d01a614bfd4666b196bb7b4c5af6d7b82b0
blackmarllbor0 <3100194@gmail.com>
2024-05-15 22:13:41 +0300

    feat: migrations for casbin-adapter
    Signed-off-by: blackmarllbor0 <3100194@gmail.com>

---
+++ migrations/pg/000001_casbin_init.down.sql
@@ -0,0 +1,8 @@
+drop index if exists prtype_index;
+drop index if exists v0_idx;
+drop index if exists v1_idx;
+drop index if exists v2_idx;
+
+drop table if exists casbin.casbin_rule;
+
+drop schema if exists casbin;
